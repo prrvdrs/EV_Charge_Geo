836d9648ff09f4ab8e3cb0b955bab72097611258
Pierre <prr.vdrs@gmail.com>
2019-02-11 16:19:14 +0000

    txt to csv converter

---
+++ evcp.py
@@ -0,0 +1,14 @@
+import csv
+
+# Convert .txt file to .csv:
+
+csvfile = open('evcp_201811.csv', 'w')
+writer = csv.writer(csvfile, delimiter=',', lineterminator='\n', quotechar='"')
+writer.writerow(
+    ["Date", "Time", "ID", "Type", "Status", "Coordinates", "Address", "Latitude", "Longitude"])
+
+with open('201811a.txt', 'r') as f:
+    reader = csv.reader(f, dialect='excel', delimiter='\t')
+    for row in reader:
+        print (row)
+        writer.writerow(row)
