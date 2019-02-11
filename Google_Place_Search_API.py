f2564367082c1cb533dea9523609464aca4454f2
Pierre <prr.vdrs@gmail.com>
2019-02-11 13:54:02 +0000

    Draft

--- Google_Place_Search_API.py
+++ Google_Place_Search_API.py
@@ -1,3 +1,7 @@
 import json
 
 auth = api_key
+evc_location = [] #example_= -33.8670522,151.1957362
+radius = 500 #
+
+request = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={evc_location}&radius={radius}&key={auth}"
