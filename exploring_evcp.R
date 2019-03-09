3b6c7455af056b99db2ecc0ce6d6bb91412f3add
Pierre <prr.vdrs@gmail.com>
2019-03-09 16:19:46 +0000

    update

---
+++ exploring_evcp.R
@@ -0,0 +1,14 @@
+
+library(dplyr)
+
+data <- read.csv("C:/Users/pierr/Documents/Projects/EV_Charge_Geo/data/data_evcp.txt", header= FALSE, sep = "\t")
+colnames(data) <- c("Date", "Time", "ID", "Type", "Status", "Coordinates", "Address", "Latitude", "Longitude")
+data <- data.frame(data)
+head(data)
+
+# Randomly selected location on a single day:
+test <- data %>%
+        filter(ID == 'CP:CBDG2'| Date == '20181101')
+head(test)
+
+#test_location <- data[data$ID == 'CP:CBDG2',]
